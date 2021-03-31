d472118c6148a1652408477ea1892663ccab3f86
github-VladOrpik <65450885+VladOrpik@users.noreply.github.com>
2021-03-31 20:48:48 +0300

    add routes

--- cinema-react/src/App.js
+++ cinema-react/src/App.js
@@ -1,44 +1,59 @@
 
 import './App.scss';
-import logo from './images/logo.jpg';
+import React from "react";
+import Header from './Header';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
      <Header/>
+     <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Афиша</Link>
+            </li>
+            <li>
+              <Link to="/Кинотеатры">Кинотеатры</Link>
+            </li>
+            <li>
+              <Link to="/Инфо">Инфо</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/Кинотеатры">
+            <Кинотеатры />
+          </Route>
+          <Route path="/Инфо">
+            <Инфо />
+          </Route>
+          <Route path="/">
+            <Афиша />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
     </div>
   );
 }
-function Header(){
-  return(
-    <header className="header">
-      <div className="wrapper">
-        <div className="header__inner">
-        <div className="header__logo">
-          <a className="header__logo-img" href="#">
-            <img className="logo" src={logo} alt="my-logo" />
-          </a>
-        </div>
-          <nav className="header__menu">
-            <ul className="header__menu-list">
-              <li className="header__menu-item">
-                <a  href="#"className="header__menu-link">Афиша</a></li>
-              <li className="header__menu-item">
-                <a href="#" className="header__menu-link">Кинотеатры</a></li>
-              <li className="header__menu-item">
-                <a href="#" className="header__menu-link">Инфо</a></li>
-              <li className="header__menu-item">
-                <button className="header__menu-sign-in"><span className="text__button">Sign in</span></button>
-              </li>
-              <li className="header__menu-item">
-              <button className="header__menu-sign-up"> <span className="text__button">Sign up</span></button>
-              </li>
-            </ul>
-          </nav>
-      </div>
-      </div>
-    </header>
-  );
+function Афиша() {
+  return <h2>Афиша</h2>;
+}
+
+function Кинотеатры() {
+  return <h2>Кинотеатры</h2>;
+}
+
+function Инфо() {
+  return <h2>Кинотеатры</h2>;
 }
 
 export default App;
---
+++ cinema-react/src/Header.js
@@ -0,0 +1,33 @@
+import logo from './images/logo.jpg';
+function Header(){
+    return(
+      <header className="header">
+        <div className="wrapper">
+          <div className="header__inner">
+          <div className="header__logo">
+            <a className="header__logo-img" href="#">
+            <img className="logo" src={logo} alt="my-logo" />
+            </a>
+          </div>
+            <nav className="header__menu">
+              <ul className="header__menu-list">
+                <li className="header__menu-item">
+                  <a  href="Афиша"className="header__menu-link">Афиша</a></li>
+                <li className="header__menu-item">
+                  <a href="Кинотеатры" className="header__menu-link">Кинотеатры</a></li>
+                <li className="header__menu-item">
+                  <a href="Инфо" className="header__menu-link">Инфо</a></li>
+                <li className="header__menu-item">
+                  <button className="header__menu-sign-in"><span className="text__button">Sign in</span></button>
+                </li>
+                <li className="header__menu-item">
+                <button className="header__menu-sign-up"> <span className="text__button">Sign up</span></button>
+                </li>
+              </ul>
+            </nav>
+        </div>
+        </div>
+      </header>
+    );
+  }
+  export default Header;
